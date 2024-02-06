87db47daf5284ab0d7df2193ca9fc1f24d7a74f2
prasadl502 <154116631+prasadl502@users.noreply.github.com>
2024-02-06 15:43:49 +0530

    added https logic

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
-const express = require('express');
-const app = express()
-const PORT = 3001
-const custom = process.env.CUSTOM || 'custom'
 
-let iCount = 0;
+const https = require("https"); 
 
-app.get('/', (req, res) => {
-  res.send(`Hello from App Server`)
+const express = require("express"); 
+const app = express(); 
+
+const fs = require("fs"); 
+const bodyParser = require("body-parser"); 
+
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
+
+app.get("/", function (req, res) { 
+  res.send("Hello from app server."); 
+}); 
+
+app.post("/cloudTrail", function (req, res) { 
+  console.log(req.body); 
+}); 
+
+const options = { 
+key: fs.readFileSync("server.key"), 
+cert: fs.readFileSync("server.cert"), 
+}; 
+
+
+https.createServer(options, app) 
+.listen(3000, function (req, res) { 
+console.log("Server started at port 3000"); 
 });
-
-app.post('/clouflareTrial', (req, res) => {
-  console.log("Hit count : " + iCount);
-  iCount++;
-  res.set('x-custom-response', 'cloudflareTrialEndpoint');
-  res.send({
-    "x-custom-body": "custom_request_body",
-    'response': 'Hello post',
-  });
-});
-
-
-app.post('/clouflareTrial2', (req, res) => {
-  console.log("Hit count : " + iCount);
-  iCount++;
-  res.set('x-custom-response', 'cloudflareTrialEndpoint2');
-  res.send({
-    "x-custom-body": "custom_request_body",
-    'response': 'Hello post',
-  });
-});
-
-
-app.listen(PORT, () => {
-console.log(`app running on http://localhost:${PORT}`)});
